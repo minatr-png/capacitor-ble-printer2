9f552f066e9e77d906fdbb4b1aad9980438e52e8
mnavarro <mnavarro@ahora.es>
2026-07-29 14:28:56 +0200

    Upgraded to allow compatibility with capacitor 8

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,7 @@ import { registerPlugin } from '@capacitor/core';
 
 import type { ZPLConverterPlugin } from './definitions';
 
-const ZPLPrinter = registerPlugin<ZPLConverterPlugin>('ZPLPrinter', {
-  web: () => import('./web').then(m => new m.ZPLPrinterWeb()),
-});
+const ZPLPrinter = registerPlugin<ZPLConverterPlugin>('ZPLPrinter');
 
 export * from './definitions';
 export { ZPLPrinter };
